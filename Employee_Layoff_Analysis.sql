ed977e993724797775319028ad66f80d4227fd0b
Ramesh kumar M <100695925+rameshkumarm95@users.noreply.github.com>
2024-09-15 10:45:24 +0530

    Update Employee_Layoff_Analysis.sql

--- Employee_Layoff_Analysis.sql
+++ Employee_Layoff_Analysis.sql
@@ -1,40 +1,6 @@
 -- Database
 USE SQL_Portfolio_Projects;
 
-/* 
-Project_name: Employee Layoff Analysis
-
-Dataset: (https://www.kaggle.com/datasets/paiky1995/warn-layoffs-dataset)
-
-About Dataset
-
-The federal WARN Act requires large employers to give advance
-notice of layoffs to state governments and workers. States publish
-this layoff information with varying degrees of specificity.
-
-The dataset contains 73030 rows and 16 columns
-
-Columns:
-
-1.  State				: U.S. state where the layoff occurred.
-2.  Company				: Name of the company.
-3.  City				: City where the layoff occurred (missing values).
-4.  Number of Workers	: Number of workers affected (some missing values).
-5.  Received Date		: Date the layoff notice was received.
-6.  Effective Date		: Date the layoff was effective (many missing values).
-7.  Closure / Layoff	: Type of action (closure or layoff).
-8.  Temporary/Permanent	: Type of layoff (temporary or permanent).
-9.  Union				: Whether a union was involved (mostly missing).
-10. Region				: The region where the layoff happened (many missing values).
-11. County				: County information (many missing values).
-12. Industry			: Industry of the company (many missing values).
-13. Notes				: Additional notes on the layoff.
-14. Date_of_Scrape		: Date the data was scraped.
-15. Parent_Company		: Parent company (mostly missing).
-16. Address				: Address of the company (mostly missing).
-
-
-*/
 
 -- View all the attributes and records
 SELECT * FROM [dbo].[Employee_Layoff_Analysis];
@@ -146,18 +112,4 @@ ORDER BY 1 DESC;
 SELECT COUNT(DISTINCT company) AS companies
 FROM Employee_Layoff_Analysis
 WHERE number_of_workers > 500;
-.
 
-/*
-This project focused on analyzing layoff data to provide insights into the layoff trends, the most affected regions, industries, and companies. 
-After cleaning the data and fixing date conversion issues, we explored several key factors, including:
-
-1 .The states and industries most affected by layoffs.
-2. Companies with the highest layoffs and the number of workers impacted.
-3. The distinction between temporary and permanent layoffs.
-4. The influence of unions and the time gap between receiving and enacting layoffs.
-
-This analysis will provide valuable insights to better understand the scope and trends of layoffs across various industries and regions, 
-helping stakeholders make informed decisions and plan mitigation strategies.
-
-*/
